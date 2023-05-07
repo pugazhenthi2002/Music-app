6abce38d605d4bfe824dff5620a1a9774ee769d3
PUGAZHENTHI THANIARASU I R <105981927+pugazhenthi2002@users.noreply.github.com>
2023-05-07 19:31:07 +0530

    Update music.cpp

--- music.cpp
+++ music.cpp
@@ -1,421 +1,376 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-vector<vector<string>> songDetails = {
-  {"Beliver","hiphop","Imagine Dragons","Album1","100","2003"},
-  {"Hello","hiphopp","Imagine Dragons ","Album2","200","2003"},
-  {"Dont let me down","melody","ChainSmokers","Album3","150","2003"},
-  {"closer","melody","ChainSmokers","Album4","120","2003"},
-  {"Alone","rock","Alan Walker","Album4","160","2003"},
-  {"Faded","rock","Alan Walker","Album5","120","2003"}
-};
 
-//songName genreType artistName albumName Year
+
 class Songs{
-public:
-static int songCount; 
-int songId;
-string songName;
-int albumId;
-int genreId;
-int artistId;
-int year;
-}songs[100];
-
-
-class Genre{
-  public:
-  static int genreCount;
-  int genreId;
-  string genreType;
-}genre[100];
-
-class Album{
-  public:
-  static int albumCount;
-  int albumId;
-  string albumName;
-  int artistId;
-  int subscription;
-  int price;
-}album[100];
-class Artist{
-  public:
-  static int artistCount;
-  int artistId;
-  string artistName;
-}artist[100];
-
-class PlayList{
-  public:
-  static int playListCount;
-  int playListId;
-  string playListName;
-  int songsCount;
-  vector<Songs>songsList;
+	public:
+		int songid;
+		string songname;
+		int artist_ID;
+		int albumid;
+		int genreid;
+		int year;
 };
-vector<PlayList>playListList;
 
-int Songs::songCount = 0;
-int Genre::genreCount = 0;
-int Album::albumCount = 0;
-int Artist::artistCount = 0;
-int PlayList::playListCount = 0;
-
-void displaySong()
+class PlayList
 {
-  system("cls");
-  for(int i=0;i<Songs::songCount;i++)
-  {
-    if(album[(songs[i].albumId)-1].subscription == true)
-    {
-      cout<<songs[i].songId<<" ";
-      cout<<songs[i].songName<<" ";
-      cout<<genre[(songs[i].genreId)-1].genreType<<" ";
-      cout<<artist[(songs[i].artistId)-1].artistName<<" ";
-      cout<<album[(songs[i].albumId)-1].albumName<<" ";
-      cout<<songs[i].albumId<<" ";
-      cout<<songs[i].artistId<<" ";
-      cout<<songs[i].year<<endl;
-    }
-  }
+	public:
+		string playlistname;
+		vector<Songs>playlistsongs;
+};
+
+class Artist{
+	public:
+		int artistID;
+		string artistname;
+		Songs* art_head;
+		Songs* art_tail;
+};
+class Album{
+	public:
+		int albumID;
+		string albumname;
+		string purchased;
+//		int artistID;
+		
+//		Songs* al_head;
+//		Songs* al_tail;
+};
+class Genre{
+	public:
+		int genreID;
+		string genreName;
+		Songs* gen_head;
+		Songs* gen_tail;
+};
+class User{
+	private:
+		string password;
+	public:
+		string username;
+		string name;
+		long long int phone_no; 
+		int userid;
+		vector<Songs>song_details;
+		vector<Album>album_details;
+		vector<Artist>artist_details;
+		vector<Genre>genre_details;
+		vector<PlayList>playlist_details;
+		static int count;
+		User()
+		{
+			count++;
+			userid=count;
+		}
+		void createpassword()
+		{
+			string re_pass;
+			cin>>password;
+			while(1)
+			{
+				cout<<"\nRe-enter the Password: ";
+				cin>>re_pass;
+				if(re_pass==password)
+				{
+					cout<<"\nAccount Created\n\n";
+					break;
+				}
+				else
+				{
+					cout<<"\nRe-enter your Password";
+				}
+			}
+		}
+		bool isauthorised(string password)
+		{
+			if(this->password==password)
+			{
+				return true;
+			}
+			return false;
+		}
+		void createsongs(Songs s[],Artist ar[],Album a[],Genre g[])
+		{
+			for(int ctr=0;ctr<15;ctr++)
+			{
+				song_details.push_back(s[ctr]);
+			}
+			for(int ctr=0;ctr<8;ctr++)
+			{
+				album_details.push_back(a[ctr]);
+			}
+			for(int ctr=0;ctr<6;ctr++)
+			{
+				artist_details.push_back(ar[ctr]);
+			}
+			for(int ctr=0;ctr<5;ctr++)
+			{
+				genre_details.push_back(g[ctr]);
+			}
+		}
+};
+vector<User>user_details;
+int User::count=0;
+
+
+
+class traverse{
+	public:
+		int songID;
+		Songs* prev_genre;
+		Songs* next_genre;	
+		Songs* prev_album;
+		Songs* next_album;
+		Songs* prev_artist;
+		Songs* next_artist;
+		Songs* prev_year;
+		Songs* next_year;	
+};
+
+void mysongs(User u)
+{
+	int key=0,flag=0;
+	for(int ctr=0;ctr<u.song_details.size();ctr++)
+	{
+		key=u.song_details[ctr].albumid;
+		if(u.album_details[key-1].purchased=="Yes")
+		{
+			flag=1;
+			cout<<u.song_details[ctr].songid<<"\t"<<u.song_details[ctr].songname<<"\t"<<u.song_details[ctr].artist_ID<<"\t"<<u.song_details[ctr].albumid<<"\t"<<u.song_details[ctr].genreid<<"\t"<<u.song_details[ctr].year<<endl;
+		}
+	}
+	if(flag==0)
+	{
+		cout<<"No Songs Available\n";
+	}
 }
 
-void buySongs()
+void albums(User& u)
 {
-  system("cls");
-  cout<<"---------Available Songs-----------"<<endl;
-  for(int i=0;i<Album::albumCount;i++)
-  {
-    if(album[i].subscription != true)
-    cout<<album[i].albumId<<"   "<<album[i].albumName<<" "<<album[i].price<<"   "<<endl;
-  }
-  cout<<"Enter the album id to buy the album"<<endl;
-  int albumId;
-  while(1)
-  {
-    cin>>albumId;
-    if(albumId >Album::albumCount)
-    {
-      cout<<"Enter a valid Id or press 0 to exit"<<endl;
-    }
-    else if(albumId == 0)
-    {
-      break;
-    }
-    else
-    {
-      album[albumId-1].subscription = true;
-      break;
-    }
-  }
+	string token;int token_id;
+	for(int ctr=0;ctr<u.album_details.size();ctr++)
+	{
+		if(u.album_details[ctr].purchased=="No")
+		{
+			cout<<u.album_details[ctr].albumID<<"\t"<<u.album_details[ctr].albumname<<endl;
+		}
+	}
+	
+	while(1)
+	{
+		cout<<"\nDo you want to Purchase any albums?\t";
+		cin>>token;
+		
+		if(token=="No" || token=="NO")
+		{
+			break;
+		}
+		
+		cout<<"Enter the Album ID which you want to purchase: ";
+		cin>>token_id;
+		
+		u.album_details[token_id-1].purchased="Yes";
+		
+		for(int ctr=0;ctr<u.song_details.size();ctr++)
+		{
+			int x=u.song_details[ctr].albumid;
+			cout<<u.song_details[ctr].songid<<"\t"<<u.song_details[ctr].songname<<"\t"<<u.album_details[x-1].purchased<<"\t\t"<<endl;
+		}
+		
+	}
+	
+	
 }
 
-void playAlbum()
+void createplaylist(User& u)
 {
-  system("cls");
-  cout<<"---------Available Albums-----------"<<endl;
-  for(int i=0;i<Album::albumCount;i++)
-  {
-    
-    if(album[i].subscription == true)
-    {
-      cout<<album[i].albumId<<"   "<<album[i].albumName<<"   "<<endl;
-      cout<<"------------Songs in the album-----------------"<<endl;
-      for(int j=0;j<Songs::songCount;j++)
-      {
-        if(songs[j].albumId == album[i].albumId)
-        {
-          cout<<songs[j].songId<<" ";
-          cout<<songs[j].songName<<" ";
-          cout<<genre[(songs[j].genreId)-1].genreType<<" ";
-          cout<<artist[(songs[j].artistId)-1].artistName<<" ";
-          cout<<album[(songs[j].albumId)-1].albumName<<" ";
-          cout<<songs[j].year<<endl;
-        }
-      }
-    }
-    
-  }
-  cout<<"Enter the album id to play the album"<<endl;
-  int albumId;
-  
-  while(1)
-  {
-    cin>>albumId;
-    cout<<albumId<<endl;
-    if(albumId >Album::albumCount)
-    {
-      cout<<"Enter a valid Id or press 0 to exit"<<endl;
-    }
-    else if(albumId == 0)
-    {
-      break;
-    }
-    else
-    {
-      break;
-    }
-  }
-  vector<Songs>songList;
-  for(int i=0;i<Songs::songCount;i++)
-  {
-    if(songs[i].albumId == albumId)
-    {
-      songList.push_back(songs[i]);
-    }
-  }
-  int size = songList.size(), index = 0;
-  for(int i=0;i<size;i++)
-  {
-    cout<<songList[i].songName<<endl;
-  }
-  while(1)
-  {
-    cout<<songList[index].songId<<"   "<<songList[index].songName<<"   "<<artist[(songList[index].artistId)-1].artistName<<endl;
-    cout<<"1.For previous Song"<<endl;
-    cout<<"2.For next song"<<endl;
-    cout<<"3.To exxit"<<endl;
-    int choice;
-    cin>>choice;
-    if(choice >= 1 && choice <=3)
-    {
-      if(choice == 1)
-      {
-        if(index == 0)
-        {
-          cout<<"No previous song"<<endl;
-        }
-        else
-        {
-          --index;
-        }
-      }
-      else if(choice == 2)
-      {
-        if(index >= size-1)
-        {
-          cout<<"No next song"<<endl;
-        }
-        else{
-          index++;
-        }
-      }
-      else {
-        break;
-      }
-    }
-    else
-    {
-      cout<<" Enter a valid choice"s<<endl;
-    }
-  }
+	PlayList p;
+	string playlist_name;
+	
+	int key=0,flag=0;
+	for(int ctr=0;ctr<u.song_details.size();ctr++)
+	{
+		key=u.song_details[ctr].albumid;
+		if(u.album_details[key-1].purchased=="Yes")
+		{
+			flag=1;
+			cout<<u.song_details[ctr].songid<<"\t"<<u.song_details[ctr].songname<<"\t"<<u.song_details[ctr].artist_ID<<"\t"<<u.song_details[ctr].albumid<<"\t"<<u.song_details[ctr].genreid<<"\t"<<u.song_details[ctr].year<<endl;
+		}
+	}
+	
+	cout<<"Create Playlist Name:\t";
+	cin>>p.playlistname;
+	int token;
+	while(1)
+	{
+		cout<<"Add Songs to the Playlist "<<p.playlistname<<endl;
+		cout<<"Enter Song ID\n";
+		cout<<"Or Enter 0 to EXIT\n";
+		cin>>token;
+		if(token==0)
+		{
+			break;
+		}
+		p.playlistsongs.push_back(u.song_details[token-1]);
+	}
 }
 
-void createPlayList()
+void viewplaylist(User& u)
 {
-    PlayList newPlayList;
-    PlayList::playListCount++;
-    newPlayList.songsCount=0;
-    cout<<"Enter the playList name"<<endl;
-    newPlayList.playListId = PlayList::playListCount;
-    cin>>newPlayList.playListName;
-    while(1)
-    {
-      displaySong();
-      cout<<"Enter the song Id to add"<<endl;
-      cout<<"Enter 0 to exit"<<endl;
-      int choice;
-      cin>>choice;
-      if(choice)
-      {
-        for(int i=0;i<Songs::songCount;i++)
-        {
-          if(songs[i].songId == choice)
-          {
-            newPlayList.songsList.push_back(songs[i]);
-            newPlayList.songsCount++;
-            cout<<"Successfully added"<<endl;
-            break;
-          }
-        }
-      }
-      else{
-        break;
-      }
-    }
-    playListList.push_back(newPlayList);
-
+	for(int ctr=0;ctr<u.playlist_details.size();ctr++)
+	{
+		cout<<u.playlist_details[ctr].playlistname<<endl;
+		for(int ptr=0;ptr<u.playlist_details[ctr].playlistsongs.size();ptr++)
+		{
+			cout<<u.playlist_details[ctr].playlistsongs[ptr].songid<<"\t"<<u.playlist_details[ctr].playlistsongs[ptr].songname<<endl;
+		}
+		cout<<"---------------------------------------------------------------\n";
+	}
 }
 
-void playPlayList(){
-  cout<<"------------------Select a Playlist--------------------------"<<endl;
-  for(int i=0;i<PlayList::playListCount;i++)
-  {
-    cout<<playListList[i].playListId<<"   "<<playListList[i].playListName<<endl;
-  }
-  cout<<"Enter the playList id to play"<<endl;
-  int choice;
-  cin>>choice;
-  int size = playListList[choice-1].songsCount, index = 0;
-  for(int i=0;i<size;i++)
-  {
-    cout<<playListList[choice-1].songsList[i].songName<<endl;
-  }
-  while(1)
-  {
-    cout<<playListList[choice-1].songsList[index].songId<<"   "<<playListList[choice-1].songsList[index].songName<<"   "<<endl;
-    cout<<"1.For previous Song"<<endl;
-    cout<<"2.For next song"<<endl;
-    cout<<"3.To exxit"<<endl;
-    int choice;
-    cin>>choice;
-    system("cls");
-    if(choice >= 1 && choice <=3)
-    {
-      if(choice == 1)
-      {
-        if(index == 0)
-        {
-          cout<<"No previous song"<<endl;
-        }
-        else
-        {
-          --index;
-        }
-      }
-      else if(choice == 2)
-      {
-        if(index >= size-1)
-        {
-          cout<<"No next song"<<endl;
-        }
-        else{
-          index++;
-        }
-      }
-      else {
-        break;
-      }
-    }
-    else
-    {
-      cout<<" Enter a valid choice"s<<endl;
-    }
-  }
+void playlist(User& u)
+{
+int choice;	
+	while(1)
+	{
+		cout<<"-----------------------------------------\n1.\t Create Playlist\n2.\t View Playlist\n0.\t EXIT\n-----------------------------------------\n";
+		cout<<"Enter the Token:\t";
+		cin>>choice;
+		if(choice==0)
+		{
+			break;
+		}
+		switch(choice)
+		{
+			case 1: createplaylist(u);
+			case 2: viewplaylist(u);
+			default: cout<<"INVALID TOKEN\n";
+		}
+		
+	}
+	
+	
+}
+
+void login()
+{
+	string login_user,login_pass;
+	cout<<"\nUsername: ";
+	cin>>login_user;
+	cout<<"\nPassword: ";
+	cin>>login_pass;
+	int token;
+	for(int ctr=0;ctr<user_details.size();ctr++)
+	{
+		if(user_details[ctr].username==login_user && user_details[ctr].isauthorised(login_pass))
+		{
+			while(1)
+			{
+//				for(int ptr=0;ptr<user_details[ctr].song_details.size();ptr++)
+//				{
+//					cout<<user_details[ctr].song_details[ptr].songname<<endl;
+//				}
+
+				cout<<"-----------------------------------------\n1.\tMy Songs\n2.\tAlbums\n3.\tPlaylist\n-----------------------------------------\n";
+				cout<<"\nEnter the Token: ";
+				cin>>token;
+				if(token==0)
+				{
+					cout<<"\n************\tLOG OUT\t************\n\n";
+					break;
+				}
+				switch(token)
+				{
+					case 1: mysongs(user_details[ctr]);break;
+					case 2: albums(user_details[ctr]);break;
+					case 3: playlist(user_details[ctr]);break;
+					default: cout<<"\nINVALID TOKEN\n";break;
+				}
+			}			
+		}
+	}
+}
+void register_acc()
+{
+	Songs* genrehead=NULL;
+	Songs* albumhead=NULL;
+	Songs* artisthead=NULL;
+	Songs s[30];
+	s[0]={1,"Arabic Kuthu",1,1,1,2022};
+	s[1]={2,"Aathichodi",2,2,1,2011};
+	s[2]={3,"Ullaala",1,3,1,2018};
+	s[3]={4,"Petta Parakh",1,3,2,2018};
+	s[4]={5,"Marana Mass",1,3,1,2018};
+	s[5]={6,"Engayum Kadhal",3,4,3,2012};
+	s[6]={7,"Believer",4,5,4,2017};
+	s[7]={8,"Animal",4,5,4,2018};
+	s[8]={9,"SuperMan",5,6,4,2019};
+	s[9]={10,"Bones",4,5,4,2020};
+	s[10]={11,"Arabic Kadal",3,4,3,2002};
+	s[11]={12,"Hosana",3,7,3,2014};
+	s[12]={13,"Yethi Yethi",6,8,1,2015};
+	s[13]={14,"Anjala",6,8,5,2015};
+	s[14]={15,"Adiyae Kolludhae",6,8,3,2015};
+	
+	Artist a[6];
+	a[0]={1,"Anirudh"};
+	a[1]={2,"Vj Antony"};
+	a[2]={3,"ARR"};
+	a[3]={4,"Imagine Dragons"};
+	a[4]={5,"Eminem"};
+	a[5]={6,"HJ"};
+	
+	Album al[8];
+	al[0]={1,"Beast","No"};
+	al[1]={2,"VA1","No"};
+	al[2]={3,"Petta","No"};
+	al[3]={4,"EK","No"};
+	al[4]={5,"ID1","No"};	
+	al[5]={6,"EM1","No"};
+	al[6]={7,"VTV","No"};
+	al[7]={8,"V1000","No"};
+	
+	Genre g[5];
+	g[0]={1,"Kuthu"};
+	g[1]={2,"Beats"};
+	g[2]={3,"Melody"};
+	g[3]={4,"English"};
+	g[4]={5,"Sad"};
+	
+	
+	User u;
+	cout<<"\nEnter your name: ";
+	cin>>u.name;
+	cout<<"\nEnter your Phone Number: ";
+	cin>>u.phone_no;
+	cout<<"\nCreate your username: ";
+	cin>>u.username;
+	cout<<"Create Password: ";
+	u.createpassword();
+	u.createsongs(s,a,al,g);
+	user_details.push_back(u);
 }
 int main()
 {
-
-  //songName genreType artistName albumName Year
-  for(auto a:songDetails)
-  {
-    int songId = ++Songs::songCount;
-
-    //Song Id
-    songs[songId-1].songId = songId;
-    songs[songId-1].songName = a[0];
-    int flag = 0;
-
-    //Genre Id
-    int genreId;
-    for(int i=0;i<Genre::genreCount;i++)
-    {
-      if(a[1] == genre[i].genreType)
-      {
-        flag = 1;
-        genreId = genre[i].genreId;
-        break;
-      }
-    }
-    if(!flag)
-    {
-      genreId = ++Genre::genreCount;
-      genre[genreId-1].genreId = genreId;
-      genre[genreId-1].genreType = a[1];
-    }
-    songs[songId-1].genreId = genreId;
-
-    //artist Name
-    int artistId;
-    for(int i=0;i<Artist::artistCount;i++)
-    {
-      if(a[2] == artist[i].artistName)
-      {
-        flag = 1;
-        artistId = artist[i].artistId;
-        break;
-      }
-    }
-    if(!flag)
-    {
-      artistId = ++Artist::artistCount;
-      artist[artistId-1].artistId = artistId;
-      artist[artistId-1].artistName = a[2];
-    }
-    songs[songId-1].artistId = artistId;
-
-    //AlbumId
-
-    int albumid;
-    for(int i=0;i<Album::albumCount;i++)
-    {
-      if(a[3] == album[i].albumName)
-      {
-        flag = 1;
-        albumid = album[i].albumId;
-        break;
-      }
-    }
-    if(!flag)
-    {
-      albumid = ++Album::albumCount;
-      album[albumid-1].albumId = albumid; 
-      album[albumid-1].albumName = a[3]; 
-      album[albumid-1].subscription = false;
-      album[albumid-1].price = stoi(a[4]);
-    }
-    songs[songId-1].albumId = albumid;
-
-    //year
-    songs[songId-1].year = stoi(a[5]);  
-  }
- 
- while(1)
- {
-  int choice;
-  cout<<"---------------------------Spotify--------------------------"<<endl; 
-  cout<<"1.To display all the songs"<<endl;
-  cout<<"2.To buy songs"<<endl;
-  cout<<"3.To play a albumm"<<endl;
-  cout<<"4.Create a Play list"<<endl;
-  cout<<"5.Play a Play list"<<endl;
-  cout<<"6.To close the app"<<endl;
-  cout<<"Enter your choice"<<endl;
-  cin>>choice;
-  if(choice == 6)
-  {
-    break;
-  }
-  switch(choice){
-    case 1:displaySong();
-    break;
-    case 2:
-    buySongs();
-    break;
-    case 3:
-    playAlbum();
-    break;
-    case 4:
-    createPlayList();
-    break;
-    case 5:
-    playPlayList();
-    break;
-    default:
-    cout<<"Enter a valid choice"<<endl;
-    break;
-  }
- }
-  return 0;
+	
+	
+	int token;
+	while(1)
+	{
+		cout<<"-----------------------------------------\n1.\tLogin\n2.\tRegister\n0.\tExit\n-----------------------------------------\n";
+		cout<<"Enter the Token: ";	
+		cin>>token;
+		if(token==0)
+		{
+			cout<<"\n************\tTHANK YOU\t************\n\n";
+			break;
+		}
+		switch(token)
+		{
+			case 1: login();break;
+			case 2: register_acc();break;
+			default: cout<<"\nINVALID TOKEN\n\n";
+		}
+	}
 }
